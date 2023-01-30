bcc6c46be57ae833c4016c5f17fcb29b4a83914c
Winniee356 <lamwingjie@gmail.com>
2023-01-30 15:45:00 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,4 +3,5 @@ const UI = {
     am_pm: document.querySelector(`.am-pm`),
     second: document.querySelector(`.hand--second`),
     minute: document.querySelector(`.hand--minute`),
+    hour: document.querySelector(`.hand--hour`),
 }
