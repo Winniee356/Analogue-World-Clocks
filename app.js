794cc468cd13e839d42e470d6a8dc6b0e190df20
Winniee356 <lamwingjie@gmail.com>
2023-02-09 18:16:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function updateClock() {
     // getting date and time
     const now = new Date();
     const date = now.getDate();
-    const seconds = now.getSeconds();
+    const seconds = now.getSeconds() / 60 * 360;
     const minutes = now.getMinutes();
     const hours = now.getHours()
     
