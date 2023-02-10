cafd87e2b1875df08837029be22a39b2d42f02c1
Winniee356 <lamwingjie@gmail.com>
2023-02-10 14:40:10 +0100

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ function updateClock() {
     UI.date.textContent = date;
     UI.second.style.transform = `rotate(${seconds}deg)`;
     UI.minute.style.transform = `rotate(${minutes}deg)`;
-    UI.hour.style.transform = `rotate($[seconds]deg)`;
+    UI.hour.style.transform = `rotate(${hours}deg)`;
     requestAnimationFrame(updateClock)
 }
 
